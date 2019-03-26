7a6e229c7e4200bf8deff01430ba5b0829980e47
D7703_07 <d7703_07@lulzsec.local>
2019-03-26 15:46:38 +0900

    commit!!!!

--- imageView3/ViewController.swift
+++ imageView3/ViewController.swift
@@ -9,11 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myImgView: UIImageView!
+    var count = 1
+    var sw = 0
+    @IBOutlet weak var myLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myImgView.image = UIImage(named: "frame1.png")
+        myLable.text = "1"
     }
+    
+    @IBAction func changeImage(_ sender: UIButton) {
+        
+        if sw == 0 {
+            count += 1
+            if count == 5 {
+                sw = 1
+            }
+        }
+        else{
+            count -= 1
+            if count == 1 {
+                sw = 0
+            }
+        }
+            
+        
+        myImgView.image = UIImage(named: "frame\(count).png")
+        
+        myLable.text = String(count)
+    }
+    
 
 
 }
